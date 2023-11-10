cbef77205d5b94471b0c92d3e566437fef8ee9bc
Marek Żebrowski <marek.zebrowski@gmail.com>
2023-11-10 22:19:22 +0100

    Initial Commit

---
+++ InfranMac/InfranMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfranMacApp.swift
+//  InfranMac
+//
+//  Created by Marek Żebrowski on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct InfranMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InfranMacUITests/InfranMacUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  InfranMacUITestsLaunchTests.swift
+//  InfranMacUITests
+//
+//  Created by Marek Żebrowski on 10/11/2023.
+//
+
+import XCTest
+
+final class InfranMacUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
